dd7f649271b8f42fe6b160037d53a1c0c32928c9
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-30 03:30:09 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,7 +6,7 @@ class Backer
     @backed_projects = []
   end 
   
-  def self.back_project(project_arg)
+  def back_project(project_arg)
     @backed_projects << project_arg
     # project_arg.add_backer(self)
   end 
